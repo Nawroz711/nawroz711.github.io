dad94aa3d656060fdbeebbad344d880afce598ec
DESKTOP-K95QBNN\Nawroz Ali Alamdost <na.alamdost@gmail.com>
2025-11-12 12:52:11 -0800

    🎨 Complete portfolio customization

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,14 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', {delay: 400});
 sr.reveal('.home__social-icon', { interval: 200});
 sr.reveal('.skills__data, .work__img, .contact__input', {interval: 200});
+
+/*==================== EXPANDABLE NAV MENU ====================*/
+const navExpand = document.getElementById('nav-expand');
+const navMenu = document.getElementById('nav-menu');
+
+if (navExpand && navMenu) {
+    navExpand.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+        navExpand.classList.toggle('rotated');
+    });
+}
